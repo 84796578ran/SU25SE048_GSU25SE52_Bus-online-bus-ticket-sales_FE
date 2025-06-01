251a3b41c5da125713ca028da1b7dcd13f5172fa
ThanhTrung78 <TrungPTSE151481@fpt.edu.vn>
2025-06-01 23:07:57 +0700

    upload App

--- SU25SE048_GSU25SE52_Bus-online-bus-ticket-sales_FE/op/src/App.js
+++ SU25SE048_GSU25SE52_Bus-online-bus-ticket-sales_FE/op/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
-import LoginForm from './login/Login';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './header/Header';
-import RegisterForm from './register/Register';
+import RegisterForm from './API/register/Register';
 import About from './about/About';
 import Contact from './contact/Contact';
 import ForgotPassword from './forgotPassword/ForgotPass';
+import BusBooking from './layoutBanVe/BusSeatBooking';
+import GoogleLoginButton from './googleLoginButton/GoogleLoginButton';
+import HomePage from './home/HomePage';
+import ManageRoute from './pages/manager/manageRoute/ManageRoute';
+import LoginForm from './API/login/Login';
 
 function App() {
   return (
@@ -18,10 +22,13 @@ function App() {
           <Route path='/about' element={<About />} />
           <Route path='/contact' element={<Contact />} />
           <Route path='/forgotPass' element={<ForgotPassword />} />
+          <Route path='/services' element={<BusBooking />} />
+          <Route path='/home' element={<HomePage />} />
+          <Route path='/loginGoogle' element={<GoogleLoginButton />} />
+          <Route path='/manageRoute' element={<ManageRoute />} />
         </Routes>
       </BrowserRouter>
     </div>
   );
 }
-
 export default App;
