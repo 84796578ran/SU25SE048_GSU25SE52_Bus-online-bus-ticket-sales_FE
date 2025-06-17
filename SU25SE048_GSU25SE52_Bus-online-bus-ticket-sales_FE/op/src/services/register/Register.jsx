652ebaa2c5e5cf863dfa9e085f73cc7192ef2ed2
ThanhTrung78 <TrungPTSE151481@fpt.edu.vn>
2025-06-17 14:14:04 +0700

    edit Register, BusSeatBooking

--- SU25SE048_GSU25SE52_Bus-online-bus-ticket-sales_FE/op/src/services/register/Register.jsx
+++ SU25SE048_GSU25SE52_Bus-online-bus-ticket-sales_FE/op/src/services/register/Register.jsx
@@ -38,30 +38,34 @@ const RegisterForm = () => {
         <div>
             <div className='register-layout'>
                 <Header />
-                <div className='register-container'>
-                    <h2>Đăng ký</h2>
-                    <form onSubmit={handleSubmit}>
-                        <input type="text" name="name" placeholder="Họ và tên" onChange={handleChange} value={formData.name} required />
-                        <input type="email" name="email" placeholder="Email" onChange={handleChange} value={formData.email} required />
-                        <input type='text' name='phone' placeholder='Số điện thoại' onChange={handleChange} value={formData.phone} required />
-                        <input type="password" name="password" placeholder="Mật khẩu" onChange={handleChange} value={formData.password} required />
-                        <input type="password" name="confirmPassword" placeholder="Xác nhận mật khẩu" onChange={handleChange} value={formData.confirmPassword} required />
-                        {error && <p className="error-message">{error}</p>} {/* Hiển thị lỗi nếu có */}
-                        <button className="google-login" onClick={() =>
-                            window.location.href = '/loginGoogle'
-                        }>
-                            <img src="GoogleLogo.png"
-                                className="google-logo"></img>
-                            Đăng kí bằng Gmail
-                        </button>
-                        <button type="submit">Đăng ký</button>
-                        <p className='login-link'>
-                            Đã có tài khoản? <Link to='/'>Đăng nhập</Link>
-                        </p>
-                    </form>
+                <div className='content-wrapper'>
+                    <div className='register-container'>
+                        <h2>Đăng ký</h2>
+                        <form onSubmit={handleSubmit}>
+                            <input type="text" name="name" placeholder="Họ và tên" onChange={handleChange} value={formData.name} required />
+                            <input type="email" name="email" placeholder="Email" onChange={handleChange} value={formData.email} required />
+                            <input type='text' name='phone' placeholder='Số điện thoại' onChange={handleChange} value={formData.phone} required />
+                            <input type="password" name="password" placeholder="Mật khẩu" onChange={handleChange} value={formData.password} required />
+                            <input type="password" name="confirmPassword" placeholder="Xác nhận mật khẩu" onChange={handleChange} value={formData.confirmPassword} required />
+                            {error && <p className="error-message">{error}</p>} {/* Hiển thị lỗi nếu có */}
+                            <button className="google-login" onClick={() =>
+                                window.location.href = '/loginGoogle'
+                            }>
+                                <img src="GoogleLogo.png"
+                                    className="google-logo"></img>
+                                Đăng kí bằng Gmail
+                            </button>
+                            <button type="submit">Đăng ký</button>
+                            <p className='login-link'>
+                                Đã có tài khoản? <Link to='/'>Đăng nhập</Link>
+                            </p>
+                        </form>
+                    </div>
                 </div>
             </div>
-            <Footer />
+            <div className='register-footer'>
+                <Footer />
+            </div>
         </div>
     )
 }
